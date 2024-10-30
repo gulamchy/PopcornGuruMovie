e4626c66b12e6dee2c1ad8dd1b9032f07ad3d642
gulamchy <gulamsulaman@gmail.com>
2024-10-30 00:59:52 -0700

    Let's hope for the best

---
+++ backend/index.js
@@ -0,0 +1,99 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const { readFileContent } = require("./storage");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const allowedOrigins = process.env.FRONTEND_URL || "http://localhost:3000" || "*";
+
+
+
+let movies = [];
+let similarity = [];
+
+const loadDataFromGCS = async () => {
+  try {
+    movies = await readFileContent("movie.json");
+    similarity = await readFileContent("similarity.json");
+  } catch (error) {
+    console.error("Error loading data from GCS:", error);
+    throw new Error("Failed to load data");
+  }
+};
+
+loadDataFromGCS()
+  .then(() => {
+    console.log("Data loaded successfully");
+  })
+  .catch((error) => {
+    console.error("Failed to load data:", error);
+  });
+
+const getRecommendations = async (movieTitle) => {
+  const movieIndex = movies.findIndex(
+    (m) => m.title.toLowerCase() === movieTitle.toLowerCase()
+  );
+  if (movieIndex === -1) return [];
+
+  let similarities;
+  try {
+    similarities = await readFileContent("similarity.json", movieIndex);
+  } catch (error) {
+    console.error(
+      `Error fetching similarities for movie ${movieTitle}:`,
+      error
+    );
+    return [];
+  }
+
+  if (!similarities) {
+    console.error(`No similarities found for movie: ${movieTitle}`);
+    return [];
+  }
+
+  return similarities
+    .map((score, index) => ({
+      movie: movies[index],
+      score,
+    }))
+    .filter(
+      (item) =>
+        item.score > 0.1 &&
+        item.movie.title.toLowerCase() !== movieTitle.toLowerCase()
+    )
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 14)
+    .map((item) => item.movie);
+};
+
+app.get("/movies", (req, res) => {
+  if (movies.length === 0) {
+    return res.status(500).json({ error: "Movie data is not loaded" });
+  }
+  res.json(movies);
+});
+
+app.post("/recommend", async (req, res) => {
+  const { movieTitle } = req.body;
+  if (!movieTitle) {
+    return res.status(400).json({ error: "Movie title is required" });
+  }
+
+  try {
+    const recommendations = await getRecommendations(movieTitle);
+    res.json({ recommendations });
+  } catch (error) {
+    res.status(500).json({ error: "Failed to get recommendations" });
+  }
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ backend/storage.js
@@ -0,0 +1,39 @@
+const { Storage } = require("@google-cloud/storage");
+const path = require("path");
+require("dotenv").config();
+
+const keyFilename =
+  process.env.NODE_ENV === "test"
+    ? "/etc/secrets/elegant-moment-440103-f0-53cb3a480423.json"
+    : path.join(__dirname, "./elegant-moment-440103-f0-53cb3a480423.json");
+
+const storage = new Storage({
+  keyFilename: keyFilename,
+
+  projectId: "elegant-moment-440103-f0",
+});
+
+const bucketName = "popcornmovie";
+
+async function readFileContent(fileName, lineNumber = null) {
+  try {
+    const file = storage.bucket(bucketName).file(fileName);
+
+    if (lineNumber !== null && fileName === "similarity.json") {
+      const [contents] = await file.download();
+      const data = JSON.parse(contents.toString("utf-8"));
+
+      return data[lineNumber] || [];
+    } else {
+      const [contents] = await file.download();
+      return JSON.parse(contents.toString("utf-8"));
+    }
+  } catch (error) {
+    console.error(`Error reading file ${fileName}:`, error);
+    throw new Error("Failed to read file from GCS");
+  }
+}
+
+module.exports = {
+  readFileContent,
+};
